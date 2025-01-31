96151a4dbae66780191ed67df59b1666fb1cd8be
PlamenRog <plamen.github@gmail.com>
2025-01-31 18:52:22 +0200

    did some debugging

--- src/main.c
+++ src/main.c
@@ -2,90 +2,106 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <ctype.h>
 
 char* getInput()
 {
 	char *input = NULL;
-    size_t len = 0;
-    ssize_t nread;
+	size_t len = 0;
+	ssize_t nread;
 
+	nread = getline(&input, &len, stdin);
 
-    nread = getline(&input, &len, stdin);
+	if (nread == -1) {
+		perror("Error reading input");
+		return NULL;
+	}
 
-    if (nread == -1) {
-        perror("Error reading input");
-        return "";
-    }
-
-//  printf("You entered: %s", input);
+//	printf("You entered: %s", input);
 	return input;
 }
 
-char* parseInput(char* str)
+char *trimwhitespace(char *str)
 {
-    char *delim = " ";
-    char *token;
-    char **tokens;
-    int i = 0, tokenCount = 0;
+	char *end;
 
-    char *strCopy = (char *)malloc(strlen(str) + 1);
-    if (strCopy == NULL) {
-        printf("Memory allocation failed for strCopy!\n");
-        return "";
-    }
-    strcpy(strCopy, str);
+	while(isspace((unsigned char)*str)) str++;
 
-    token = strtok(strCopy, delim);
-    while (token != NULL) {
-        tokenCount++;
-        token = strtok(NULL, delim);
-    }
+	if(*str == 0)
+		return str;
 
-    tokens = (char **)malloc(tokenCount * sizeof(char *));
-    if (tokens == NULL) {
-        printf("Memory allocation failed for tokens array!\n");
-        free(strCopy);
-        return "";
-    }
+	end = str + strlen(str) - 1;
+	while(end > str && isspace((unsigned char)*end)) end--;
 
-    token = strtok(str, delim);
-    while (token != NULL) {
-        tokens[i] = token;
-        i++;
-        token = strtok(NULL, delim);  // Get the next token
-    }
+	end[1] = '\0';
 
-    for (int j = 0; j < tokenCount; j++) {
-        printf("tokens[%d]: %s\n", j, tokens[j]);
-    }
-
-    free(strCopy);
-    free(tokens);
+	return str;
 }
-/*
+
+char** parseInput(char* str)
+{
+	char *delim = " ";
+	char *token;
+	char **tokens;
+	int i = 0, tokenCount = 0;
+
+	char *strCopy = (char *)malloc(strlen(str) + 1);
+	if (strCopy == NULL) {
+		printf("Memory allocation failed for strCopy!\n");
+		return NULL;
+	}
+	strcpy(strCopy, str);
+
+	token = strtok(strCopy, delim);
+	while (token != NULL) {
+		tokenCount++;
+		token = strtok(NULL, delim);
+	}
+
+	tokens = (char**)malloc(tokenCount * sizeof(char *));
+	if (tokens == NULL) {
+		printf("Memory allocation failed for tokens array!\n");
+		free(strCopy);
+		return NULL;
+	}
+
+	token = strtok(str, delim);
+	while (token != NULL) {
+		tokens[i] = token;
+		i++;
+		token = strtok(NULL, delim);
+	}
+
+	free(strCopy);
+		
+    for (int i = 0; tokens[i] != NULL; i++) {
+        printf("arguments[%d]: %s\n", i, tokens[i]);
+    }
+	return tokens;
+}
+
 int main(void)
 {
 	while (1)
 	{
-
+		// read
 		char* defaultPrompt = "rsh> ";
 		printf("%s", defaultPrompt);
-		char* input = getInput();
 
-		execl("/bin/ls", "ls", (char *)NULL);
+		char* input = trimwhitespace(getInput());
+
+		// parse
+		char** arguments = parseInput(input);
+		if (arguments == NULL) return 1;
+		//int numberOfArguments = sizeof(arguments) / sizeof(*arguments);
 
 
-	    free(input);
 
+		// eval
+		//execl("/bin/ls", "ls", (char *)NULL);
+
+		// loop
 	}
 
-	return EXIT_SUCCESS;
-}
-*/
-
-int main(void)
-{
-	char input[] = "temporary test string";
-	parseInput(input);
 	return EXIT_SUCCESS;
 }
