44a4a11d70e55e1682f837d4bee8857b61e23512
PlamenRog <plamen.github@gmail.com>
2025-01-31 14:49:34 +0200

    implemented input function

--- src/main.c
+++ src/main.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+char* getInput()
+{
+	char *input = NULL;
+    size_t len = 0;
+    ssize_t nread;
+
+
+    nread = getline(&input, &len, stdin);
+
+    if (nread == -1) {
+        perror("Error reading input");
+        return "";
+    }
+
+//  printf("You entered: %s", input);
+	return input;
+}
+
+
 int main(void)
 {
 
